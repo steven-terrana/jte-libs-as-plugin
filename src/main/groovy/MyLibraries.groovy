3cd1df45ba0684def3b3bdc718afd97028f0e1c9
steven-terrana <steventerrana@gmail.com>
2021-10-07 19:40:42 -0400

    Update MyLibraries.groovy

--- src/main/groovy/MyLibraries.groovy
+++ src/main/groovy/MyLibraries.groovy
@@ -5,7 +5,6 @@
 */
 package customize_this
 
-
 import org.boozallen.plugins.jte.init.governance.libs.LibraryProvidingPlugin
 import org.boozallen.plugins.jte.init.governance.libs.LibraryProvidingPlugin.LibraryProvidingPluginDescriptor
 import hudson.Extension
